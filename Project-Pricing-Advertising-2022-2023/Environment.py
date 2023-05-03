dea13ea057caa09cc2651d507f652273c600bebd
Andrea Riboni <Ar.riboni.andrea@gmail.com>
2023-05-03 20:19:24 +0200

    update cumulative function

--- Project-Pricing-Advertising-2022-2023/Environment.py
+++ Project-Pricing-Advertising-2022-2023/Environment.py
@@ -8,7 +8,7 @@ class UserClass():
         self.f1 = f1
         self.f2 = f2
         self.name = name
-# In the context of bidding in online advertising, a function that represents the
+    # In the context of bidding in online advertising, a function that represents the
     # dependence between the number of clicks and the bid should be bounded
     # because it is not realistic to expect an unlimited number of clicks regardless
     # of the bid value
@@ -26,14 +26,20 @@ class UserClass():
 
 class Environment():
     def __init__(self):
-        self.classes = [UserClass('C1',0,0), UserClass('C2',0,1), UserClass('C3',1,0)]
+        self.classes = [
+            UserClass('C1',0,0),
+            UserClass('C2',0,1),
+            UserClass('C3',1,0)
+            ]
+
     def generate_observations(self, noise_std, bid, index):
         func = self.classes[index].get_click_bids(bid)
         return func + np.random.normal(0, noise_std, size = func.shape)
 
     def get_cumulative_cost(self, bid, noise_std, index):
         clicks = self.classes[index].get_click_bids(bid)
-        avg_cost = np.cumsum(clicks) #/ np.arange(1, len(clicks) + 1) ** 0.5  # define a concave average cost curve
+        avg_cost = clicks * 2.5
+        #avg_cost = np.cumsum(clicks) / np.arange(1, len(clicks) + 1) ** 0.725  # define a concave average cost curve
         return avg_cost + np.random.normal(0, noise_std, size=avg_cost.shape)  # add Gaussian noise to the average cost
 
     #def get_cumulative_cost(self, bid, noise_std):
@@ -41,7 +47,7 @@ class Environment():
     #    return np.cumsum(clicks + np.random.normal(0, noise_std, size=clicks.shape))
 
 env = Environment()
-n_obs = 50
+n_obs = 1 #just for simplicity
 noise_std = 5.0
 bids = np.linspace(0.0, 2.50, 20)
 # Initialize arrays to store observed bids and clicks
@@ -49,7 +55,7 @@ x_obs = np.array([])
 y_obs = np.array([])
 clicks_per_bid = []
 # Generate observations by randomly selecting bids and adding noise to the expected clicks
-for i in range(0, 20):
+for i in range(0, n_obs):
     new_x_obs = np.random.choice(bids, 1)
     new_y_obs = env.generate_observations(noise_std, new_x_obs, 0)
 
@@ -76,7 +82,7 @@ for i in range(0, 20):
     # Plot the results, including the true click values, the observed click values, the predicted click values,
     # and the 95% confidence intervals
     #plt.figure(i)
-    #plt.plot(x_pred, env.get_cumulative_cost(x_pred, noise_std,0), 'r:', label = r'$C1_CS$')
+    plt.plot(x_pred, env.get_cumulative_cost(x_pred, noise_std,0), 'r:', label = r'$C1_{CS}$')
     plt.plot(x_pred, (1.0 - np.exp(-10.0 * x_pred)) * 200, 'g:', label=r'$C1$')
     #plt.plot(x_pred, (1.0 - np.exp(-3.0 * x_pred)) * 50, 'b:', label=r'$C3$')
     #plt.plot(X.ravel(), Y, 'ro', label = u'Observed Clicks')
