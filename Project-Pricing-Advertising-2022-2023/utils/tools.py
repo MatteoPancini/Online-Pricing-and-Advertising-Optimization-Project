6bf5181ddbe9d41af5b6166dd443dea350a6701f
Valeria <valeria.amato@mail.polimi.it>
2023-07-09 23:57:46 +0200

    refactor UCB and Learner

--- Project-Pricing-Advertising-2022-2023/utils/tools.py
+++ Project-Pricing-Advertising-2022-2023/utils/tools.py
@@ -7,3 +7,4 @@ def calculate_price_index(price):
 def fun(user_class, bid, price):
     mean_per_bid = user_class.get_click_bids(bid) * (user_class.get_conversion_per_price(price) * calculate_margin(price)) - user_class.get_click_bids(bid) * user_class.get_cost_per_click(bid)    #sample_cost
     return mean_per_bid
+
