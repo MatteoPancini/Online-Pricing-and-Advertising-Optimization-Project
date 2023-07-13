214516c79dfffe0926bd0cd88347685f13c95c5f
Andrea Riboni <Ar.riboni.andrea@gmail.com>
2023-07-13 15:28:40 +0200

    s4s2 refactor

--- Project-Pricing-Advertising-2022-2023/p4/ContextManager.py
+++ Project-Pricing-Advertising-2022-2023/p4/ContextManager.py
@@ -2,15 +2,14 @@ import pandas as pd
 import numpy as np
 import pandas as pd
 from p4.Multi_TS_Learner import *
-import parameters as param
-
 
 class ContextManager:
-    def __init__(self, features_names, features):
-        self.features_names = features_names
-        self.features = features
-        self.samples = pd.DataFrame(
-            columns=[*self.features_names, 'bid', 'price', 'n_conversions', 'n_clicks', 'cum_costs', 'reward'])
+    def __init__(self):
+        self.features_names = ['f1', 'f2']
+        self.features = ['00', '01', '10', '11'] #11 probably not necessary
+        self.samples = pd.DataFrame(columns=[*self.features_names, 'bid', 'price', 'n_conversions', 'n_clicks', 'cum_costs', 'reward'])        
+        self.prices = [50, 100, 150, 200, 250]
+        self.bids = np.linspace(0.01, 3.0, 100)
 
     def update(self, new_samples):
         new_samples_df = pd.DataFrame(new_samples,
@@ -18,16 +17,16 @@ class ContextManager:
                                                'cum_costs',
                                                'reward'])
         self.samples = pd.concat([self.samples, new_samples_df])
-
+    
     def get_context(self):  # convert list of dictionaries into list of lists
         context_dict = self.get_context_recursive()
         context_list = []
         if len(context_dict) == 0:
-            return [param.feature_combos]
+            return [self.features]
         for dictionary in context_dict:
             class_list = []
-            for i in range(len(param.features_names)):
-                if param.features_names[i] not in dictionary.keys():
+            for i in range(2): #for each feature
+                if self.features_names[i] not in dictionary.keys():
                     class_list_copy = class_list.copy()
                     if len(class_list_copy) == 0:
                         class_list = [['0'], ['1']]
@@ -40,14 +39,14 @@ class ContextManager:
                             class_list.remove(class_name_list)
                 else:
                     if len(class_list) == 0:
-                        class_list = [[str(dictionary[param.features_names[i]])]]
+                        class_list = [[str(dictionary[self.features_names[i]])]]
                     else:
                         for class_name_list in class_list:
-                            class_name_list.append(str(dictionary[param.features_names[i]]))
+                            class_name_list.append(str(dictionary[self.features_names[i]]))
             context_list.append([''.join(class_name_list) for class_name_list in class_list])
         return context_list
 
-    def get_context_recursive(self, features=param.features_names, samples=None, lower_bound_mean=None):
+    def get_context_recursive(self, features=['f1', 'f2'], samples=None, lower_bound_mean=None):
         if len(features) == 0:
             return []
         features_names_to_be_split = features.copy()
@@ -56,12 +55,10 @@ class ContextManager:
             conversion_per_price = samples.groupby(['price'])['n_conversions'].sum().astype(float)
             n_clicks_per_price = samples.groupby(['price'])['n_clicks'].sum().astype(float)
             conversion_rate_per_price = conversion_per_price.divide(n_clicks_per_price).astype(float)
-            lb_per_price = conversion_rate_per_price - np.sqrt(
-                np.array(-np.log(param.confidence) / (2 * n_clicks_per_price)).astype(float))
+            lb_per_price = conversion_rate_per_price - np.sqrt(np.array(-np.log(0.95) / (2 * n_clicks_per_price)).astype(float)) #Hoeffding bound with a confidence of 0.95 (slide 29)
             lb_pricing_probability = lb_per_price.max()
             price_max_idx = lb_per_price.idxmax()
-            margin = param.prices[price_max_idx] - param.cost
-
+            margin = self.prices[price_max_idx] - self.prices[price_max_idx] * 0.3
             n_clicks_per_bid = samples.groupby(['bid'])['n_clicks']
             cum_costs_per_bid = samples.groupby(['bid'])['cum_costs']
             n_clicks_per_bid_lb = n_clicks_per_bid.mean().to_numpy() - 1.96 * n_clicks_per_bid.std().fillna(
@@ -108,21 +105,21 @@ class ContextManager:
                     sub_context[max_feature] = 1
             return context_0 + context_1
         return []
-
+    
     def get_lower_bounds(self, filtered_samples, total_samples):
         n_samples_filtered = np.sum(filtered_samples['n_clicks'].to_numpy())
         n_samples_total = np.sum(total_samples['n_clicks'].to_numpy())
         lb_probability = n_samples_filtered / n_samples_total - np.sqrt(
-            -np.log(param.confidence) / (2 * n_samples_filtered))
+            -np.log(0.95) / (2 * n_samples_filtered))
 
         conversion_per_price = filtered_samples.groupby(['price'])['n_conversions'].sum().astype(float)
         n_clicks_per_price = filtered_samples.groupby(['price'])['n_clicks'].sum().astype(float)
         conversion_rate_per_price = conversion_per_price.divide(n_clicks_per_price).astype(float)
         lb_per_price = conversion_rate_per_price - np.sqrt(
-            np.array(-np.log(param.confidence) / (2 * n_clicks_per_price)).astype(float))
+            np.array(-np.log(0.95) / (2 * n_clicks_per_price)).astype(float))
         lb_pricing_probability = lb_per_price.max()
         price_max_idx = lb_per_price.idxmax()
-        margin = param.prices[price_max_idx] - param.cost
+        margin = self.prices[price_max_idx] - self.prices[price_max_idx] * 0.3
 
         n_clicks_per_bid = filtered_samples.groupby(['bid'])['n_clicks']
         cum_costs_per_bid = filtered_samples.groupby(['bid'])['cum_costs']
@@ -140,12 +137,12 @@ class ContextManager:
         for feature_combo in context:
             filtered_samples_per_feature = self.samples
             features = list(feature_combo)
-            for i in range(len(param.features_names)):
+            for i in range(len(self.features_names)):
                 filtered_samples_per_feature = filtered_samples_per_feature.loc[
-                    filtered_samples_per_feature[param.features_names[i]] == features[i]]
+                    filtered_samples_per_feature[self.features_names[i]] == features[i]]
             filtered_samples = pd.concat([filtered_samples, filtered_samples_per_feature])
         return [filtered_samples[column].to_numpy() for column in filtered_samples.columns[2:]]
-
+    
 
 class ContextOptimizer:
     def __init__(self, optimizer_type):
@@ -154,47 +151,63 @@ class ContextOptimizer:
         param.features_combos = [00, 01, 10, 11]
         optimizer_type: classe (MultiLearner: TS / UCB)
         """
-        self.context_generator = ContextManager(param.features_names, param.feature_combos) #crea il context generator con la lista di feature e le possibili combinazioni
+        self.context_generator = ContextManager() #crea il context generator con la lista di feature e le possibili combinazioni
         self.optimizer_type = optimizer_type #assegna il learner
+        self.prices = [50, 100, 150, 200, 250]
+        self.bids = np.linspace(0.01, 3.0, 100)
         self.context_wise_learner = {
-            tuple(param.feature_combos): self.optimizer_type(param.bids, param.prices) #initializes one learner per context (each feature combination creates a context)
+            tuple(['00', '01', '10', '11']): self.optimizer_type(self.bids, self.prices) #initializes one learner per context (each feature combination creates a context)
         }
         self.T = 0
         self.collected_rewards = []
 
     def pull_arms(self):
         bids_and_prices = {}
+        """
+        ad ora esiste solo un contesto "00 01 10 11"
+        l'arm viene pullata e assegnata a tutti quei contesti
+        """
         for context in self.context_wise_learner.keys(): #for each possible context (feature combination)
+            #print(f"iterating on contexts. Now the context is {context}")
             arms = self.context_wise_learner[context].pull_arms() #pull arm
+            #print(f"\tpulled arms: {arms}")
             for feature in context:
+                #print(f"\titerating on features. Now {feature}")
                 bids_and_prices[feature] = arms
         return bids_and_prices
 
     def update(self, input_per_feature):
-        self.T += 1
-        # update
+        self.T += 1 #time step increment
+        #iterates on each context
         for context in self.context_wise_learner.keys():
+            """
+            at this point
+            - context = [00, 01, 10, 11]
+            - input_per_feature contains [bid_index, price_index, #conversions, @clicks, total costs, reward] for each context
+            for simplicity, we save the single value into their own variables
+            """
             pulled_bids_arm = input_per_feature[context[0]][0]
             pulled_prices_arm = input_per_feature[context[0]][1]
             n_conversion_per_context = sum(input_per_feature[feature][2] for feature in context)
             n_clicks_per_context = sum(input_per_feature[feature][3] for feature in context)
             cum_cost_per_context = sum(input_per_feature[feature][4] for feature in context)
             reward_per_context = sum(input_per_feature[feature][5] for feature in context)
-            self.context_wise_learner[context].update(pulled_bids_arm, pulled_prices_arm, n_conversion_per_context,
-                                                n_clicks_per_context, cum_cost_per_context, reward_per_context)
-        self.collected_rewards.append(sum(input_per_feature[feature][5] for feature in param.feature_combos))
-        self.context_generator.update(
-            [tuple(feature) + input_per_feature[feature] for feature in input_per_feature.keys()])
-        # generate contexts
+            #now we update the learner for this context
+            self.context_wise_learner[context].update(pulled_bids_arm, pulled_prices_arm, n_conversion_per_context, n_clicks_per_context, cum_cost_per_context, reward_per_context)
+        #and we collect the reward for each context. The total reward is just the sum
+        self.collected_rewards.append(sum(input_per_feature[feature][5] for feature in ['00', '01', '10', '11']))
+        self.context_generator.update([tuple(feature) + input_per_feature[feature] for feature in input_per_feature.keys()])
+        
+        #each two weeks we have to run the context generation algorithm
         if self.T % 14 == 0:
-            context_structure = [tuple(i) for i in self.context_generator.get_context()]
+            context_structure = [tuple(i) for i in self.context_generator.get_context()] #we generate the context structure, aka -------------------------------------------------------
             keys = list(self.context_wise_learner.keys()).copy()
             for context in keys:
                 if context not in context_structure:
                     del self.context_wise_learner[context]
             for context in context_structure:
                 if context not in keys:
-                    self.context_wise_learner[tuple(context)] = self.optimizer_type(param.bids, param.prices)
+                    self.context_wise_learner[tuple(context)] = self.optimizer_type(self.bids, self.prices)
                     # retrieve samples
                     samples = self.context_generator.get_samples(context)
                     # bulk update learner
--- Project-Pricing-Advertising-2022-2023/p4/feature_based_environment.py
+++ Project-Pricing-Advertising-2022-2023/p4/feature_based_environment.py
@@ -1,29 +1,32 @@
-import parameters as param
 import numpy as np
+from utils.User_Classes import UserClass
 
 
 class Environment:
     def __init__(self, feature):
         self.feature = feature
-        self.pricing_probabilities = param.pricing_probabilities[param.class_from_feature[self.feature]]
-        self.optimal_price_idx = np.argmax(self.pricing_probabilities * (param.prices - param.cost))
-        self.n_clicks = np.round(param.n_clicks_per_bid_by_feature[self.feature](param.bids)).astype(np.int32)
-        self.cum_costs = param.cum_cost_per_bid_by_feature[self.feature](param.bids)
-        self.optimal = np.max(self.pricing_probabilities[self.optimal_price_idx] * self.n_clicks * (
-                param.prices[self.optimal_price_idx] - param.cost) - self.cum_costs)
-        self.optimal_bid_idx = np.argmax(self.pricing_probabilities[self.optimal_price_idx] * self.n_clicks * (
-                param.prices[self.optimal_price_idx] - param.cost) - self.cum_costs)
-        self.n_bids_arms = len(param.bids)
-        self.n_prices_arms = len(param.prices)
+        self.user_class = self.compute_class_from_features(self.feature)
+        self.prices = np.array([50, 100, 150, 200, 250])
+        self.bids = np.linspace(0.01, 3.0, 100)
+        self.n_bids_arms = len(self.bids)
+        self.n_prices_arms = len(self.prices)
+        self.pricing_probabilities = self.user_class.get_conversion_probabilities()
+        self.optimal_price_idx = np.argmax(self.pricing_probabilities * (self.prices - self.prices * 0.3)) #TODO check this optimal formula
+        self.n_clicks = self.user_class.get_click_bids(self.bids)
+        self.cum_costs = self.user_class.get_total_cost(self.bids)#param.cum_cost_per_bid_by_feature[self.feature](param.bids)
+        self.optimal = np.max(self.pricing_probabilities[self.optimal_price_idx] * self.n_clicks * ( #TODO: checks this optimal formula
+                self.prices[self.optimal_price_idx] - self.prices[self.optimal_price_idx] * 0.3) - self.cum_costs)
+        self.optimal_bid_idx = np.argmax(self.pricing_probabilities[self.optimal_price_idx] * self.n_clicks * ( #TODO: checks this optimal formula
+                self.prices[self.optimal_price_idx] - self.prices[self.optimal_price_idx] * 0.3) - self.cum_costs)
 
     def round(self, pulled_bids_arm, pulled_prices_arm):
-        bid = param.bids[pulled_bids_arm]
-        price = param.prices[pulled_prices_arm]
-        n_clicks = np.round(max(0, param.n_clicks_per_bid_by_feature[self.feature](bid) + np.random.normal(0,
-                                                                                                           param.n_clicks_per_bid_sigma))).astype(
-            np.int32)
-        cum_cost = max(0, param.cum_cost_per_bid_by_feature[self.feature](bid) + np.random.normal(0,
-                                                                                                  param.cum_cost_per_bid_sigma))
+        bid = self.bids[pulled_bids_arm]
+        price = self.prices[pulled_prices_arm]
+        n_clicks = np.round(max(0,self.user_class.get_click_bids(bid) + np.random.normal(0, 10))).astype(np.int32)
+        cum_cost = max(0, self.user_class.get_total_cost(bid) + np.random.normal(0, 200))
         result = np.random.binomial(1, self.pricing_probabilities[pulled_prices_arm], n_clicks)
-        reward = np.sum(result) * (price - param.cost) - cum_cost
+        reward = np.sum(result) * (price - price*0.3) - cum_cost
         return np.sum(result), n_clicks, cum_cost, reward
+    
+    def compute_class_from_features(self, f):
+        return UserClass(f1=f[0], f2=f[1])
