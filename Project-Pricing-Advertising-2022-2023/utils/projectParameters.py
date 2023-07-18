09b117678c0863703891b78a28f1fc18deeb66f2
Emanuele Paci <100219115+emanuelePaci@users.noreply.github.com>
2023-07-18 18:56:15 +0200

    Update projectParameters.py

--- Project-Pricing-Advertising-2022-2023/utils/projectParameters.py
+++ Project-Pricing-Advertising-2022-2023/utils/projectParameters.py
@@ -34,6 +34,19 @@ pricing_probabilities_per_phase = { # User C1
     # price * probability [20, 25, 18, 16, 12.5]
 }
 
+<<<<<<< Updated upstream
+=======
+pricing_probabilities_per_phase_6_2 = { # User C1
+    1: np.array([0.25, 0.35, 0.25, 0.15, 0.10]),  # Phase 1: Regular behaviours (best price is the middle one, April) -> 150€
+    # price * probability [12.5, 35, 37.5, 30, 25]
+    2: np.array([0.1, 0.20, 0.3, 0.4, 0.25]),  # Phase 2: Holiday Season (higher price, middle one as good gift, Christmas) -> 200€
+    # price * probability [5, 20, 45, 80, 62.5]
+    3: np.array([0.4, 0.25, 0.12, 0.08, 0.05]),  # Phase 3: Sale Season (second lowest price best one, September) -> 100€
+    # price * probability [20, 25, 18, 16, 12.5]
+    4: np.array([0.8, 0.3, 0.2, 0.08, 0.1]),
+    5: np.array([0.2, 0.05, 0.12, 0.08, 0.4]),
+}
+>>>>>>> Stashed changes
 
 n_clicks_per_bid_per_class = {
     1: lambda x: (1.0 - np.exp(-5.0*x)) * 200,
