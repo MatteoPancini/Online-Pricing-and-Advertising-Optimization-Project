d6932213337731130d7856354b700230efaa9d41
Andrea Riboni <Ar.riboni.andrea@gmail.com>
2023-07-19 11:03:38 +0200

    6 scenario 1

--- Project-Pricing-Advertising-2022-2023/p6/EXP3_learner.py
+++ Project-Pricing-Advertising-2022-2023/p6/EXP3_learner.py
@@ -5,27 +5,75 @@ REFERENCES
 """
 import numpy as np
 from utils.learners.Learner import Learner
+import random
+from math import sqrt, log
+import utils.parameters as param
 
 class EXP3(Learner):
-    def __init__(self, n_arms, gamma = 5e-6):
+    def __init__(self, n_arms, upperbound_total_reward=1.0, reward_min=0.0, reward_max=1.0):
+        """
+        Implements a learner following the EXP3 algorithm
+
+        :var gamma: parameters for the ...
+        """
         super().__init__(n_arms)
-        self.n_arms = n_arms
-        self.gamma = gamma
         self.weights = np.ones(n_arms)
-        self.rewards = np.zeros(n_arms)
-    
+        self.reward_min = reward_min  # the min of all the price phases
+        self.reward_max = reward_max  # the max of all price phases
+        upperbound_total_reward_scaled = (upperbound_total_reward - self.reward_min) / (self.reward_max - self.reward_min)
+        exponential = 2.7182818284
+        self.gamma = min(1.0, sqrt(n_arms * log(n_arms) / (upperbound_total_reward_scaled * (exponential - 1))))
+        # self.gamma = np.sqrt(2*log(n_arms)/(n_arms*upperbound_total_reward))
+
     def pull_arm(self):
-        #print(f"weights(nan)={np.sum(np.isnan(self.weights))}")
-        probabilities = (1 - self.gamma) * self.weights / np.sum(self.weights) + self.gamma / self.n_arms
-        #print(f"{np.sum(np.isnan(probabilities))}")
-        arm = np.random.choice(np.arange(self.n_arms), p=probabilities)
-        return arm
-    
+        """
+        Choose the arm to pull as a random draw form  a discrete distribution
+
+        :return: number of the arm to pull
+        """
+
+        probability_distribution = distr(self.weights, self.gamma)
+        idx = draw(probability_distribution)
+        return idx
+
     def update(self, pulled_arm, reward):
-        self.t +=1
-        self.update_observations(pulled_arm, reward)
-        self.rewards[pulled_arm] += reward
-        estimate = self.rewards[pulled_arm] / (self.weights[pulled_arm] + 1e-10) # add epsilon to avoid division by zero
-        self.weights[pulled_arm] *= np.exp(self.gamma * estimate / self.n_arms)
-        self.weights /= np.sum(self.weights) # normalize weights to prevent overflow
-        #print(f"weight factor: {np.exp(self.gamma * estimate / self.n_arms)}\tweight: {self.weights[pulled_arm]}")
+        """
+        Update history of observations and weights of the arm
+
+        :param pulled_arm: number of the arm pulled at the last time step
+        :param reward: (binary) reward obtained at the last time step
+        """
+        self.t += 1
+
+        scaled_reward = ((reward[0] / (reward[0] + reward[1]) * (
+                param.prices[pulled_arm] - param.prices[pulled_arm] * 0.3)) - self.reward_min) / (
+                                self.reward_max - self.reward_min)  # rewards scaled to 0,1
+
+        probability_distribution = distr(self.weights, self.gamma)
+        estimated_reward = 1.0 * scaled_reward / probability_distribution[pulled_arm]
+        self.weights[pulled_arm] *= np.exp(
+            estimated_reward * self.gamma / self.n_arms)
+        self.update_observations(pulled_arm, reward[2])
+
+
+# draw: [float] -> int
+# pick an index from the given list of floats proportionally
+# to the size of the entry (i.e. normalize to a probability
+# distribution and draw according to the probabilities).
+
+
+def draw(weights):
+    choice = random.uniform(0, sum(weights))
+    choice_index = 0
+
+    for weight in weights:
+        choice -= weight
+        if choice <= 0:
+            return choice_index
+
+        choice_index += 1
+
+
+def distr(weights, gamma=1.0):
+    the_sum = float(sum(weights))
+    return tuple((1.0 - gamma) * (w / the_sum) + (gamma / len(weights)) for w in weights)
--- Project-Pricing-Advertising-2022-2023/p6/Environment6_1.py
+++ Project-Pricing-Advertising-2022-2023/p6/Environment6_1.py
@@ -1,48 +1,53 @@
+import utils.parameters as base_param
 import utils.projectParameters as param
+from utils.clairvoyant_tools import get_optimal_parameters
 import numpy as np
 import math
 
 
 class Environment6_1:
-    def __init__(self, user_class_id, T):
-        self.user_class_id = user_class_id
-        self.pricing_prob_per_phase = param.pricing_probabilities_per_phase
+    def __init__(self, class_id, T):
+        self.class_id = class_id
+        self.probabilities = param.pricing_probabilities_per_phase
         self.T = T
-        self.n_phases = 3
+        self.n_phases = len(self.probabilities.keys())
         self.phase_size = math.ceil(T / self.n_phases)
-
-        self.n_clicks = np.round(param.n_clicks_per_bid_per_class[self.user_class_id](param.bids)).astype(np.int32)
-        self.tot_costs = param.total_cost_per_bid_per_class[self.user_class_id](param.bids)
-        
-        self.opt_price = {}
-        self.opt_price_idx = {}
-        self.opt_bid_idx = {}
-
+        _, optimum_bid, _ = get_optimal_parameters(class_index=class_id)
+        self.n_clicks = np.round(param.n_clicks_per_bid_per_class[self.class_id](optimum_bid)).astype(np.int32)
+        self.cum_costs = param.total_cost_per_bid_per_class[self.class_id](optimum_bid)
+        self.optimal_price_idx = {}
+        self.optimal = {}
         self.n_arms = len(param.prices)
+        for phase in range(1, self.n_phases + 1):
+            self.optimal_price_idx[phase] = np.argmax(self.probabilities[phase]*(param.prices-param.cost))
+            self.optimal[phase] = self.probabilities[phase][self.optimal_price_idx[phase]] * self.n_clicks * (param.prices[self.optimal_price_idx[phase]] - param.cost) - self.cum_costs
+
+    def round(self, pulled_arm, t):
+        phase = min(math.floor(t / self.phase_size) + 1, self.n_phases)
+        result = np.random.binomial(1, self.probabilities[phase][pulled_arm], self.n_clicks)
+        reward = np.sum(result) * (param.prices[pulled_arm] - param.cost) - self.cum_costs
+        return np.sum(result), self.n_clicks - np.sum(result), reward, result
+
+    def get_opt(self, t):
+        phase = min(math.floor(t / self.phase_size) + 1, self.n_phases)
+        return self.optimal[phase]
+
+    def compute_reward_max(self): #not used
+
+        return np.max(list(self.optimal.values()))
+
+    def compute_clairvoyant_upperbound(self): # not used
+
+        clairvoyant_upperbound = self.phase_size*(sum(list(self.optimal.values())))
+
+        return clairvoyant_upperbound
+
+    def compute_reward_min(self): # not used
+        optimal = {}
+        optimal_price_idx = {}
 
         for phase in range(1, self.n_phases + 1):
-            self.opt_price_idx[phase] = np.argmax(self.pricing_prob_per_phase[phase]*(param.prices-param.cost))
-            self.opt_price[phase] = np.max(self.pricing_prob_per_phase[phase][self.opt_price_idx[phase]] * self.n_clicks * (
-                        param.prices[self.opt_price_idx[phase]] - param.cost) - self.tot_costs)
+            optimal_price_idx[phase] = np.argmin(self.probabilities[phase]*(param.prices-param.cost))
+            optimal[phase] = self.probabilities[phase][optimal_price_idx[phase]] * self.n_clicks * (param.prices[optimal_price_idx[phase]] - param.cost) - self.cum_costs
 
-            self.opt_bid_idx[phase] = np.argmax(self.pricing_prob_per_phase[phase][self.opt_price_idx[phase]] * self.n_clicks *
-                                                (param.prices[self.opt_price_idx[phase]] - param.cost) - self.tot_costs)
-            
-    def round(self, pulled_arm, t): 
-        phase = min(math.floor(t / self.phase_size) + 1, self.n_phases)
-        result = np.random.binomial(1, self.pricing_prob_per_phase[phase][pulled_arm], self.n_clicks)
-        reward = np.sum(result) * (param.prices[pulled_arm] - param.cost) - self.tot_costs 
-        return np.sum(result), self.n_clicks - np.sum(result), reward, result 
-
-    #def round(self, pulled_price_arm, pulled_bid_arm, t):
-        phase = min(math.floor(t / self.phase_size) + 1, self.n_phases)
-
-        result = np.random.binomial(1, self.pricing_prob_per_phase[phase][pulled_price_arm], self.n_clicks[pulled_bid_arm]) # Simulation of the clicks of the users
-
-        reward = np.sum(result) * (param.prices[pulled_price_arm] - param.cost) - self.tot_costs[pulled_bid_arm] # Calculation of rewards
-
-        return np.sum(result), self.n_clicks[pulled_bid_arm] - np.sum(result), reward, result   # Users who clicked, users who didn't click, reward, users who clicked (1) and users who didn't click (0)
-
-    def get_optimal_price(self, t):
-        phase = min(math.floor(t / self.phase_size) + 1, self.n_phases)
-        return self.opt_price[phase]
+        return np.min(list(optimal.values()))
