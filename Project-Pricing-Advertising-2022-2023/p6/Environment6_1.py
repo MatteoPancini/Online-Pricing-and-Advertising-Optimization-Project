e9e9e0d608db36a2e5d7643ca4340e459c7e2c3b
Andrea Riboni <Ar.riboni.andrea@gmail.com>
2023-07-20 16:04:11 +0200

    other fixes

--- Project-Pricing-Advertising-2022-2023/p6/Environment6_1.py
+++ Project-Pricing-Advertising-2022-2023/p6/Environment6_1.py
@@ -12,6 +12,7 @@ class Environment6_1:
         self.n_phases = len(self.pricing_probabilities_per_phase.keys())
         self.phase_size = math.ceil(T / self.n_phases)
         _, optimum_bid, _ = get_optimal_parameters(class_index=class_id)
+        print(f"bid {optimum_bid}")
         self.n_clicks = np.round(param.n_clicks_per_bid_per_class[self.class_id](optimum_bid)).astype(np.int32)
         self.cum_costs = param.total_cost_per_bid_per_class[self.class_id](optimum_bid)
         self.optimal_price_idx = {}
@@ -19,8 +20,8 @@ class Environment6_1:
         self.n_arms = len(param.prices)
         
         for phase in range(1, self.n_phases + 1):
-            self.optimal_price_idx[phase] = np.argmax(self.pricing_probabilities_per_phase[phase]*(param.prices-param.cost))
-            self.optimal[phase] = self.pricing_probabilities_per_phase[phase][self.optimal_price_idx[phase]] * self.n_clicks * (param.prices[self.optimal_price_idx[phase]] - param.cost) - self.cum_costs
+            self.optimal_price_idx[phase] = np.argmax(self.pricing_probabilities_per_phase[phase]*(np.array(param.prices) * 0.7))
+            self.optimal[phase] = self.pricing_probabilities_per_phase[phase][self.optimal_price_idx[phase]] * self.n_clicks * (param.prices[self.optimal_price_idx[phase]] * 0.7) - self.cum_costs
 
     def round(self, pulled_arm, t):
         phase = min(math.floor(t / self.phase_size) + 1, self.n_phases)
